4a0902577657b9e39fe84a24ca301af0eac3547e
Anuoluwapo <jaguarjum@gmail.com>
2022-11-23 13:12:54 +0100

    debug

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
-    <SparklineComponent
-    id={id}
-    height={height}
-    width={width}
-    linewidth={1}
-    valueType='Numeric'
-    fill={color}
-    border={{ color: currentColor, width: 2 }}
-    dataSource={data}
-    xName="x"
-    yName="y"
-    type={type}
-    tooltipSettings={{
-      visible: true,
-      format: '${x} : data ${y}',
-      trackLineSettings: {
-        visible: true,
-      }
-    }}
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
     >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  )
-}
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
 
-export default SparkLine
+export default Stacked;
